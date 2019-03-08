cc73aeea1b0433ccf980482397dfa7d90464b1f3
gqm <>
2019-03-08 11:08:53 +0800

    郭启宁第一次提交

---
+++ src/yaninst.java
@@ -0,0 +1,11 @@
+/**
+ * fileName:yaninst
+ * description:
+ * author:zz
+ * createTime:2019/3/8 11:07
+ */
+public class yaninst {
+    public static void main(String[] args) {
+        System.out.println("郭启明行健分子");
+    }
+}
