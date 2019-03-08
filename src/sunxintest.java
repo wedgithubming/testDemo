0326160369bb24892ca6e25d048bc8cb4beccfcd
Monroe <1187272122@qq.com>
2019-03-08 11:01:28 +0800

    孙鑫首次提交

---
+++ src/sunxintest.java
@@ -0,0 +1,11 @@
+/**
+ * fileName:sunxintest
+ * description:
+ * author: Monroe
+ * createTime:2019/3/8 10:58
+ */
+public class sunxintest {
+    public static void main(String[] args) {
+        System.out.println("这是孙鑫的测试提交");
+    }
+}
